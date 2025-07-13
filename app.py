2f03203c2eb69de44a6c2a37d39b1990141d931c
YsD9954 <yashdoke1234@gmail.com>
2025-07-14 01:37:57 +0530

    Fix for vercel static site

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from werkzeug.utils import secure_filename
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins="*")
 
 app.config['PROPAGATE_EXCEPTIONS'] = True  # Show internal errors if any
 
